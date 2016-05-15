f439af7e98986385caf7edc76e1d8d784d769798
vidim888 <vasved@ttu.ee>
2016-05-15 16:38:11 +0300

    Tehtud ühikus.

--- src/gomoku/strategies/VadimAndEvaStrategy.java
+++ src/gomoku/strategies/VadimAndEvaStrategy.java
@@ -6,7 +6,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.function.BiConsumer;
 
 /**
  * Important!
@@ -79,12 +80,162 @@ public class VadimAndEvaStrategy implements ComputerStrategy {
         }
     }
 
+    private static int width;
+
+    private static int height;
+
+    private static int empty;
+
+    private static int ourPlayer;
+
+    private static int otherPlayer;
+
+    private static int MAX_DEPTH = 2;
+
+    private static double tempScore;
+
+    private static int[] coords = new int[2];
+
+    private static double recursionLoop(int depth, int[][] board) {
+        if (depth >= MAX_DEPTH) {
+            return estimateFunction(board);
+        }
+        double minMax;
+        if (depth % 2 == 0) {
+            minMax = Double.MIN_VALUE;
+        } else {
+            minMax = Double.MAX_VALUE;
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (board[i][j] == empty) {
+                    if (depth % 2 == 0) {
+                        board[i][j] = ourPlayer;
+                        double estimated = recursionLoop(depth + 1, board);
+                        board[i][j] = empty;
+                        if (estimated > minMax) {
+                            minMax = estimated;
+                            if (depth == 0) {
+                                coords[0] = i;
+                                coords[1] = j;
+                            }
+                        }
+                    } else {
+                        board[i][j] = otherPlayer;
+                        double estimated = recursionLoop(depth + 1, board);
+                        board[i][j] = empty;
+                        if (estimated < minMax) {
+                            minMax = estimated;
+                            if (depth == 0) {
+                                coords[0] = i;
+                                coords[1] = j;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return minMax;
+    }
+
+    private static double estimateFunction(int[][] board) {
+        BiConsumer<ArrayList<Integer>, Integer> analyzer = (ourList, ourIndex) -> {
+            tempScore = 0;
+            /*for (int i = 0; i < 5; i++) {
+                int ourCounter = 0;
+                int otherCounter = 0;
+                for (int j = i; j < i + 5; j++) {
+                    if (ourList.get(j) == ourPlayer) {
+                        ourCounter++;
+                    } else if (ourList.get(j) == otherPlayer) {
+                        otherCounter++;
+                    }
+                }
+                if (ourCounter == 5) {
+                    tempScore = Double.MAX_VALUE;
+                    break;
+                } else if (otherCounter == 5) {
+                    tempScore = Double.MIN_VALUE;
+                    break;
+                }
+                tempScore += Math.pow(10, ourCounter);
+                tempScore -= Math.pow(10, otherCounter);
+            }*/
+            
+        };
+        ArrayList<Integer> eachRow;
+        double scoreToReturn = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                for (int diagonals = 0; diagonals < 4; diagonals++) {
+                    eachRow = new ArrayList<Integer>();
+                    int indexOfCenter = 4;
+                    int beforeOrAfter = 0;
+                    for (int k = -4; k <= 4; k++) {
+                        boolean wasInField = true;
+                        if (diagonals == 0) {
+                            if (i + k >= 0 && i + k < height && j + k >= 0 && j + k < width) {
+                                eachRow.add(board[i + k][j + k]);
+                            } else {
+                                wasInField = false;
+                            }
+                        } else if (diagonals == 1) {
+                            if (i + k >= 0 && i + k < height) {
+                                eachRow.add(board[i + k][j]);
+                            } else {
+                                wasInField = false;
+                            }
+                        } else if (diagonals == 2) {
+                            if (i - k >= 0 && i - k < height && j - k >= 0 && j - k < width) {
+                                eachRow.add(board[i - k][j - k]);
+                            } else {
+                                wasInField = false;
+                            }
+                        } else if (diagonals == 3) {
+                            if (j + k >= 0 && j + k < width) {
+                                eachRow.add(board[i][j + k]);
+                            } else {
+                                wasInField = false;
+                            }
+                        }
+                        if (!wasInField) {
+                            if (k < 0) {
+                                beforeOrAfter = -1;
+                            } else if (k > 0) {
+                                beforeOrAfter = 1;
+                            }
+                        }
+                    }
+                    if (beforeOrAfter == -1) {
+                        analyzer.accept(eachRow, eachRow.size() - 4);
+                    } else {
+                        analyzer.accept(eachRow, 4);
+                    }
+                    if (tempScore == Double.MAX_VALUE || tempScore == Double.MIN_VALUE) {
+                        return tempScore;
+                    }
+                    scoreToReturn += tempScore;
+                }
+            }
+        }
+        return scoreToReturn;
+    }
+
     @Override
     public Location getMove(SimpleBoard board, int player) {
-        int width = board.getWidth();
-        int height = board.getHeight();
+        width = board.getWidth();
+        height = board.getHeight();
+        empty = SimpleBoard.EMPTY;
+        ourPlayer = player;
+        if (SimpleBoard.PLAYER_BLACK == player) {
+            otherPlayer = SimpleBoard.PLAYER_WHITE;
+        } else {
+            otherPlayer = SimpleBoard.PLAYER_BLACK;
+        }
         int[][] b = board.getBoard();
-        if (myBoard == null) {
+        recursionLoop(0, b);
+        return new Location(coords[0], coords[1]);
+        /*if (myBoard == null) {
             myBoard = b;
         } else {
             int lastTurn[] = new int[2];
@@ -156,7 +307,7 @@ public class VadimAndEvaStrategy implements ComputerStrategy {
             }
         }
         myBoard[bestCoord[0]][bestCoord[1]] = player;
-        return new Location(bestCoord[0], bestCoord[1]);
+        return new Location(bestCoord[0], bestCoord[1]);*/
     }
 
     @Override
